5f058fe9b1af24727496a8cebf85dd2f7e52c904
konnenhannaford <konnen23@gmail.com>
2022-01-19 00:08:43 +1100

    updated strucutres and app.js?

--- konnen-hannaford-react-portfolio/src/App.js
+++ konnen-hannaford-react-portfolio/src/App.js
@@ -1,37 +1,46 @@
+// is this just the recsact logo
 import logo from './logo.svg';
+// do i want the css here?
 import './App.css';
 import React from "react";
 
+// how i have my protfolio structured
 import Contact from "./components/Contact";
 import Portfolio from "./components/Portfolio";
 import Landing from "./components/Landing";
 import Home from "./components/Home";
 
+// how tute has it structured
 import Projects from "./components/Projects";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Navigation from "./components/Navigation";
 
 
-export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// DO I WANT IT LIKE THIS?
+    // // tute has it as
+        //   // export default function App() {
+          
+    // // class has it as 
+        //   function App() {
+        //     return (
+        //     <div className="App">
+        //       <header className="App-header">
+        //         <img src={logo} className="App-logo" alt="logo" />
+        //         <p>
+        //           Edit <code>src/App.js</code> and save to reload.
+        //         </p>
+        //         <a
+        //           className="App-link"
+        //           href="https://reactjs.org"
+        //           target="_blank"
+        //           rel="noopener noreferrer"
+        //         >
+        //           Learn React
+        //         </a>
+        //       </header>
+        //     </div>
+        //   );
+        // }
 
 export default App;
--- konnen-hannaford-react-portfolio/src/index.js
+++ konnen-hannaford-react-portfolio/src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// do i want this or comes from mine?
+// import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// dont think i need this or test
+// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
+// dont know if its needed
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// dont think i need this or test
+
+  // // If you want to start measuring performance in your app, pass a function
+  // // to log results (for example: reportWebVitals(console.log))
+  // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  // reportWebVitals();
