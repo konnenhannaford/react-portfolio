ba945d6bc27e520af9934fa5d2cf2cb4e0e15dce
konnenhannaford <konnen23@gmail.com>
2022-01-18 23:40:54 +1100

    added components direct

--- konnen-hannaford-react-portfolio/src/App.js
+++ konnen-hannaford-react-portfolio/src/App.js
@@ -1,7 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
+import Contact from "./components/Contact";
+import Portfolio from "./components/Portfolio";
+import Landing from "./components/Landing";
+import Home from "./components/Home";
+
+import Projects from "./components/Projects";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Navigation from "./components/Navigation";
+
+
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
