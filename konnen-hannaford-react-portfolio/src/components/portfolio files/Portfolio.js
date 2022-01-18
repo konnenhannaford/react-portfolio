6d74e2ab6920fe1dc38e6f2159a842df649dbf2a
konnenhannaford <konnen23@gmail.com>
2022-01-19 04:59:06 +1100

    updated portfolio

--- konnen-hannaford-react-portfolio/src/components/portfolio files/Portfolio.js
+++ konnen-hannaford-react-portfolio/src/components/portfolio files/Portfolio.js
@@ -1,100 +1,102 @@
-import slide1Img from '../../images/crypto.png'
+import slide1Img from '../../images/quiz.png'
 import slide2Img from '../../images/crypto.png'
-import slide3Img from '../../images/crypto.png'
-import slide4Img from '../../images/crypto.png'
+import slide3Img from '../../images/Note taker.png'
+import slide4Img from '../../images/planner.png'
+import slide5Img from '../../images/socialnetwork.png'
+import slide6Img from '../../images/codepic.png'
 
 export default function Portfolio() {
     return (
             <div>
                 <div className="slideshow-container">
-
-                <div className="mySlides">
-                    <img src= {slide1Img} />
-                    <div >
-                    <p className="text">
-                        CryptoWorld
-                    </p> 
-                    <p>
-                        <a href="https://github.com/Etta0311/Group-project-1">Github Link</a>
-                    </p>
-                    <p>
-                        <a href="https://etta0311.github.io/Group-project-1/">Deployed Site</a>
-                    </p>
-                </div>
-                </div>
-
-                <div className="mySlides">
-                <img src= {slide2Img} />
-                <div >
-                    <p className="text">
-                        Password Generator
-                    </p> 
-                    <p>
-                        <a href="https://github.com/konnenhannaford/password-generator">Github Link</a>
-                    </p>
-                    <p>
-                        <a href="https://konnenhannaford.github.io/password-generator/">Deployed Site</a>
-                    </p>
-                </div>
-                </div>
-
-                <div className="mySlides">
-                <img src= {slide3Img} />
-                <div >
-                    <p className="text">
-                        Javascript Quiz
-                    </p>
-                    <p>
-                        <a href="https://github.com/konnenhannaford/Java-Quizz">Github Link</a>
-                    </p>
-                    <p>
-                        <a href="https://konnenhannaford.github.io/Java-Quizz/">Deployed Site</a>
-                    </p>
-                </div>
-                </div>
-
-                <div className="mySlides">
-                <img src= {slide4Img} />
-                <div>
+                    <div className="mySlides">
+                        <img src= {slide1Img} />
+                        <div >
                         <p className="text">
-                            Workday Scheduler
+                            Java Quizz
                         </p> 
                         <p>
-                            <a href="https://github.com/konnenhannaford/Day-Planner"> Github Link</a>
+                            <a href="https://github.com/konnenhannaford/Java-Quizz" className="fa fa-github"></a>
                         </p>
                         <p>
-                            <a href="https://konnenhannaford.github.io/Day-Planner/"> Deployed Site: </a>
-                            
+                            <a href="https://konnenhannaford.github.io/Java-Quizz/" className="fa fa-desktop" aria-hidden="true"></a>
                         </p>
-                </div>
-                </div>
+                    </div>
+                    <div className="mySlides">
+                        <img src= {slide2Img} />
+                        <div >
+                            <p className="text">
+                                CryptoWorld
+                            </p> 
+                            <p>
+                            <a href="https://github.com/Etta0311/CryptoWorld" className="fa fa-github"></a>
+                            </p>
+                            <p>
+                            <a href="https://etta0311.github.io/Group-project-1/" className="fa fa-desktop" aria-hidden="true"></a>
+                            </p>
+                        </div>
+                    </div>
 
-                <a className="prev fa fa-angle-double-left" 
-                // onClick="plusSlides(-1)"
-                ></a>
-                <a className="next fa fa-angle-double-right" 
-                // onClick="plusSlides(1)"
-                ></a>
-                </div>
-                <br/>
+                    <div className="mySlides">
+                        <img src= {slide3Img} />
+                        <div >
+                            <p className="text">
+                                Note Taker
+                            </p>
+                            <p>
+                            <a href="https://github.com/konnenhannaford/Note-Taker" className="fa fa-github"></a>
+                        </p>
+                        <p>
+                            <a href="https://lit-atoll-31253.herokuapp.com/" className="fa fa-desktop" aria-hidden="true"></a>
+                            </p>
+                        </div>
+                    </div>
 
-                <div>
-                <span className="dot" 
-                // onClick="currentSlide(1)"
-                ></span>
-                <span className="dot" 
-                // onClick="currentSlide(2)"
-                ></span>
-                <span className="dot" 
-                // onClick="currentSlide(3)"
-                ></span>
-                <span className="dot" 
-                // onClick="currentSlide(4)"
-                ></span>
-
-
-
-            </div>
+                    <div className="mySlides">
+                        <img src= {slide4Img} />
+                    <div>
+                            <p className="text">
+                                Workday Scheduler
+                            </p> 
+                            <p>
+                            <a href="https://github.com/konnenhannaford/Day-Planner" className="fa fa-github"></a>
+                        </p>
+                        <p>
+                            <a href="https://konnenhannaford.github.io/Day-Planner/" className="fa fa-desktop" aria-hidden="true"></a>
+                                
+                            </p>
+                    </div>
+                    <div className="mySlides">
+                        <img src= {slide5Img} />
+                        <div >
+                            <p className="text">
+                                Social Network API
+                            </p> 
+                            <p>
+                            <a href="https://github.com/konnenhannaford/social-network-api" className="fa fa-github"></a>
+                            </p>
+                            <p>
+                            <a href="https://watch.screencastify.com/v/9PdB2Unjo8uldkJKceG1" className="fa fa-file-video-o" aria-hidden="true"></a>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="mySlides">
+                        <img src= {slide6Img} />
+                        <div >
+                            <p className="text">
+                                Read-Me-Generator
+                            </p> 
+                            <p>
+                            <a href="https://github.com/konnenhannaford/Read-Me-Generator" className="fa fa-github"></a>
+                            </p>
+                            <p>
+                            <a href="https://watch.screencastify.com/v/BKItKpOcXESryMfuri5v" className="fa fa-file-video-o" aria-hidden="true"></a>
+                            </p>
+                        </div>
+                    </div>
+                </div> 
             </div> 
+        </div> 
+    </div> 
 )
 }
